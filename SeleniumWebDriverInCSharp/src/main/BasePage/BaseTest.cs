770061341b114953394d84c063bdd2afe9bcfc1c
ravi.jogiya <ravi.jogiya@softwebsolutions.com>
2018-07-20 16:28:30 +0530

    test

---
+++ SeleniumWebDriverInCSharp/src/main/BasePage/BaseTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using RelevantCodes.ExtentReports;
+
+namespace SeleniumWebDriverInCSharp.BasePage
+{
+    [TestFixture]
+    class BaseTest
+    {
+        public ExtentReports extent;
+        public ExtentTest log;
+        public IWebDriver driver;
+
+        public BaseTest() { }
+
+        [OneTimeSetUp]
+        public void StartReport()
+        {
+            driver = new ChromeDriver("C:\\Program Files");
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.gmail.com";
+
+            string path = System.Reflection.Assembly.GetCallingAssembly().CodeBase;
+            string actualPath = path.Substring(0, path.LastIndexOf("bin"));
+            string projectPath = new Uri(actualPath).LocalPath;
+            string reportPath = projectPath + "Reports\\TestReport.html";
+
+            extent = new ExtentReports(reportPath, true);
+            extent
+            .AddSystemInfo("Host Name", "Automation")
+            .AddSystemInfo("Environment", "QA")
+            .AddSystemInfo("User Name", "Ravi Jogiya");
+            extent.LoadConfig(projectPath + "extent-config.xml");
+        }
+
+        [TearDown]
+        public void GetResult()
+        {
+            var status = TestContext.CurrentContext.Result.Outcome.Status;
+            var stackTrace = "<pre>" + TestContext.CurrentContext.Result.StackTrace + "</pre>";
+            var errorMessage = TestContext.CurrentContext.Result.Message;
+
+            if (status == TestStatus.Failed)
+            {
+                ITakesScreenshot ts = (ITakesScreenshot)driver;
+                Screenshot screenshot = ts.GetScreenshot();
+                string pth = System.Reflection.Assembly.GetCallingAssembly().CodeBase;
+                string finalpth = pth.Substring(0, pth.LastIndexOf("bin")) + "Reports\\" + new DateTime().ToString("yyyyMMddHHmmss") + ".png";
+                string localpath = new Uri(finalpth).LocalPath;
+                screenshot.SaveAsFile(localpath, ScreenshotImageFormat.Png);
+                log.Log(LogStatus.Fail, log.AddScreenCapture(localpath));
+                log.Log(LogStatus.Fail, stackTrace + errorMessage);
+            }
+            extent.EndTest(log);
+        }
+
+        [OneTimeTearDown]
+        public void EndReport()
+        {
+            driver.Close();
+            extent.Flush();
+            extent.Close();
+
+        }
+    }
+}
---
+++ SeleniumWebDriverInCSharp/src/test/LoginTC/LoginTC.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using RelevantCodes.ExtentReports;
+using SeleniumWebDriverInCSharp.BasePage;
+namespace SeleniumWebDriverInCSharp.LoginTC
+{
+    class LoginTC : BaseTest
+    {
+
+
+        [Test]
+        public void DemoReportPass()
+        {
+            
+        
+
+
+            log = extent.StartTest("DemoReportPass");
+            Assert.IsTrue(true);
+            log.Log(LogStatus.Pass, "1.Assert Pass as condition is True");
+
+            log.Log(LogStatus.Pass, "2.Title is verifyed");
+
+            log.Log(LogStatus.Pass, "3.Title is verifyed");
+
+            log.Log(LogStatus.Pass, "4.Title is verifyed");
+        }
+
+        [Test]
+        public void DemoReportFail()
+        {
+            log = extent.StartTest("DemoReportFail");
+            Assert.IsTrue(false);
+            log.Log(LogStatus.Pass, "Assert Fail as condition is False");
+        }
+    }
+}
